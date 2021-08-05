2c675c4f66c7c0ae80007ace6b8fcc8ab3f66466
cmcouto-silva <cmcouto.silva@gmail.com>
2021-08-05 04:16:29 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,189 @@
+import kmeans
+import templates
+
+import numpy as np
+import pandas as pd
+from sklearn.datasets import make_blobs
+
+import streamlit as st	
+import streamlit.components.v1 as components
+
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="K-means Visualization App", page_icon="python.png", layout="wide")
+
+st.markdown("""
+**Autor:** Cainã Max Couto da Silva  
+**LinkedIn:** [cmcouto-silva](https://www.linkedin.com/in/cmcouto-silva/)
+""")
+
+st.header('**Visualizando o algorimo K-means etapa por etapa com Python**')
+st.write("")
+
+# components.html('<b>texto</b>')
+# st.markdown("<h1 style='text-align: center; color: red;'>texto</h1>", unsafe_allow_html=True)
+
+st.sidebar.title('Parâmetros')
+
+with st.sidebar.beta_container():
+   _, slider_col, _ = st.beta_columns([0.02, 0.96, 0.02])
+   with slider_col:
+        k = st.sidebar.select_slider(
+				label='Número de grupos (clusters) simulados:',
+				options=range(2,11), value=2
+			)
+        std = st.sidebar.slider(
+				'Desvio padrão dos dados:',
+				0.1, 5.0, 1.0, 0.1
+			)
+
+mode = st.sidebar.selectbox('Modo de inicialização dos clusters:', ["random", "kmeans++"])
+
+_, central_button, _ = st.sidebar.beta_columns([0.25, 0.5, 0.25])
+with central_button:
+	st.text("")
+	st.button('Recomputar')
+
+st.markdown("""
+Para melhor compreensão da técnica, visualize seu passo a passo abaixo.
+
+Experimente modificar a quantidade de amostras, clusters e desvio padrão das amostras. Observe que, em geral, 
+quanto menor o número de grupos e menor o desvio padrão da amostra, mais rápido se alcança a convergência da posição final dos centroides.
+O algoritmo kmeans++ também tende a acelerar a convergência dos centroides.  
+
+""")
+
+with st.beta_expander(label='Ler explicação do método'):
+	
+	st.markdown("""
+	K-means é um modelo de machine learning não supervisionado que visa identificar grupos (*i.e.* clusters) no conjunto de dados. Assim como todo modelo não supervisionado, seu objetivo é **identificar padrões nos dados** e interpretá-los, **não sendo adequado para predição**.
+
+	As variáveis utilizadas neste modelo são, **necessariamente**, numéricas. Como resultado, identificamos cada amostra à um grupo. A quantidade de grupos (K) identificados é determinada _a priori_, ou seja, antes de rodar o modelo. Contudo, existem métodos, como o método do cotovelo e da silhueta (Elbow e Silhouette, respectivamente), que nos auxiliam a identificar a melhor quantidade de grupos para nossos dados.
+
+	As variáveis categóricas podem ser utilizas após a descrição e interpretação das amostras presentes nestes grupos. **Não é recomendado** o uso de [ponderação arbitrária]() para transformar variáveis categóricas em variáveis numéricas. Ao transformar variáveis categóricas em variáveis numéricas sem respaldo da literatura, assume-se que a distância entre as categorias são as mesmas, que por sua vez pode introduzir um viés na análise.
+
+	O modelo K-means é essencialmente baseado no cálculo da distância - normalmente a [distância euclidiana]() - entre os pontos da amostra e os centroides, que são os pontos inicialmente aleatórios e representam o centro de cada grupo (cluster). Assim, a quantidade de centroides é igual ao número de grupos (estes representados pela letra "k").
+
+	Cabe ressaltar que, uma vez que K-means utiliza a distância como métrica do modelo, é necessário normalizar as dados caso as variáveis estejam em escalas diferentes.
+
+	**Em resumo, a técnica funciona assim:**
+
+	1. Definir um número de clusters (k)
+	2. Inicializar os k centroides
+	3. Categorizar cada ponto ao seu centroide mais próximo
+	4. Mover os centroides para o centro (média) dos pontos em sua categoria
+	5. Repetir as etapas 3 e 4 até as posições dos centroides não modificaram ou atingir um número máximo de iterações.  
+
+	&nbsp;
+
+	A inicialização dos centroides pode ser totalmente aleatória ou otimizada utilizando um algoritmo conhecido como K-means++.
+
+	Na inicialização aleatória, selecionamos k pontos pré-existentes como centroides, ou atribuímos k centroides dentro da dimensão dos pontos dos nosso dados. Na técnica K-means++ o objetivo é inicializar os centroides o mais distante possível um do outro, que por sua vez pode eliminar viés de inicialização de centroides (vide tópico "Armadilha do K-means") e tende a diminuir a quantidade de iterações necessárias para convergência da posição final dos centroides.
+
+	Adicionalmente, algumas aplicações também rodam mais de uma vez o algoritmo com diferentes pontos de inicialização dos centroides, fornecendo como output aquele com a menor soma das variâncias internas de cada grupo.
+	
+	_**Observações:**_
+
+	---
+
+	Os dados desta aplicação são simulados com a função [`datasets.make_blobs()`](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_blobs.html) da biblioteca [`scikit-learn`](https://scikit-learn.org/stable/index.html). Aqui, limitei tanto a quantidade de observações (100) quanto de variáveis (2), para a aplicação não ficar pesada e ser possível visualizar com gráficos bidimencionais, respectivamente. 
+
+	Não foi utilizado mais de uma corrida por k, de modo que isso pode influenciar no método do cotovelo, visto que para determinados Ks a inicialização do centroide não foi a das melhores. 
+
+	Sugestões ou críticas? Contate-me via [LinkedIn](https://www.linkedin.com/in/cmcouto-silva/).
+
+	_**Disponibilizão dos códigos:**_
+
+	---
+
+	Os scripts com a implementação passo a passo do K-means e produção deste aplicativo estão disponíveis no GitHub:
+
+	- Repositório do aplicativo
+	- Script K-means passo a passo (sem scikit-learn)
+
+	&nbsp;
+	""")
+
+data = make_blobs(centers=k, cluster_std=std)
+df = pd.DataFrame(data[0], columns=['x','y']).assign(label = data[1])
+
+if st.checkbox('Mostrar dados brutos'):
+	_, df_col, _ = st.beta_columns([0.25,0.2, 0.25])
+	with df_col:
+		st.write(df)
+
+model, wss = kmeans.calculate_WSS(data[0], k, 10, mode=mode)
+raw_col, elbow_col = st.beta_columns([0.5,0.5])
+
+_, kanimation_col, _ = st.beta_columns([0.2,0.8,0.2])
+
+with kanimation_col:
+	fig = kmeans.plot(model)
+	fig = fig.update_layout(autosize=False, height=560,
+		title_text="<b>Visualizando as etapas do K-means</b>", title_font=dict(size=24))
+	st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
+
+with raw_col:
+	raw_fig = go.Figure(
+		data=fig.data[0],
+		layout=dict(
+			template='seaborn', title='<b>Pontos sem agrupamento:</b>',
+			xaxis=dict({'title':'x'}), yaxis=dict({'title':'y'})
+			)
+		)
+	st.plotly_chart(raw_fig)
+
+with elbow_col:
+	elbow_fig = go.Figure(
+	data=go.Scatter(x=list(range(1,11)), y=wss),
+	layout=dict(
+		template='seaborn', title='<b>Método do cotovelo</b>',
+		xaxis=dict({'title':'k'}), yaxis=dict({'title':'wss'})
+		)
+	)
+	st.plotly_chart(elbow_fig)
+
+
+st.markdown("""
+---
+
+## **Armadilha do K-means**
+
+A inicialização aleatória dos centroides podem criar grupos que não representam grupos reais.
+No exemplo abaixo, percebe-se claramente a existência de quatro grupos (à esquerda), 
+equanto na animação à direita é possível observar que a iniciando os centroides próximos um dos outros,
+neste caso, levou a um agrupamento não fidedigno dos dados reais. Por este motivo normalmente se utiliza `kmeans++` e/ou
+aplica algumas vezes o modelo, ficando com aquele com menos distorção. 
+
+Por fim, é importante salientar que há outras estruturas de agrupamentos mais complexas
+onde o modelo K-means não possui bom desempenho. Nestes casos, usa-se outros modelos de agrupamento.
+Como exemplo, a primeira figura da [página de modelos de clusterização](https://scikit-learn.org/stable/modules/clustering.html) do scikit-learn 
+ilustra bem como cada modelo se sai na identificação de grupos em dados com diferentes formatos (distribuições).
+
+&nbsp;
+
+""")
+
+# Specific biased data
+raw_seed, kanimation_seed = st.beta_columns([0.5,0.5])
+
+data_seed,labels_seed = make_blobs(centers=4, random_state=3)
+model_seed = kmeans.Kmeans(data_seed, 4, seed=2)
+model_seed.fit()
+
+with raw_seed:
+	raw_fig_seed = go.Figure(
+		data=go.Scatter(x=data_seed[:,0], y=data_seed[:,1], mode='markers', marker=dict(color=labels_seed)),
+		layout=dict(title_text="<b>Pontos coloridos conforme os grupos reais</b>",
+			template="simple_white", title_font=dict(size=18)))
+	raw_fig_seed.update_layout(templates.simple_white, height=500, title_x=0.15, title_font_size=18)
+	st.plotly_chart(raw_fig_seed, use_container_width=True, sharing="streamlit")
+
+with kanimation_seed:
+	fig_seed = kmeans.plot(model_seed)
+	fig_seed = fig_seed.update_layout(autosize=False, height=500,
+		title_text="<b>Visualizando viés de inicialização dos centroides</b>", title_font=dict(size=24))
+	st.plotly_chart(fig_seed, use_container_width=True, sharing="streamlit")
+
+
+# st.markdown("""Para sugestões e críticas, favor me contatar pelo [LinkedIn](https://www.linkedin.com/in/cmcouto-silva/).""")
---
+++ kmeans.py
@@ -0,0 +1,321 @@
+import numpy as np
+import pandas as pd
+import templates
+import plotly.graph_objects as go
+
+
+class Kmeans():
+    """A class for computing K-means from scratch
+    
+    Attributes
+    ----------
+    X : numpy array
+        The input values for computing K-means
+    K : int
+        Number of clusters
+    max_iterations : int
+        Max iterations (recomputing of centroids)
+    num_individuals : int
+        Number of observations in the dataset
+    num_features : int
+        Number of variables in the dataset
+    _centroids : list
+        List of all centroids calculated in each step
+    centroids : numpy array
+        Array with final centroids
+    _labels : list
+        List of all labels calculated by each step
+    labels: numpy array
+        Array with final labels (clusters)
+    
+    Methods
+    -------
+    initialize_random_centroids()
+        Initialize centroids randomly
+    create_clusters(centroids)
+        Create K clusters by taking the minimal euclidean distance between the centroids and data points
+    calculate_new_centroids(clusters)
+        Realocate centroids to the center of each cluster
+    predict_cluster()
+        Label each observation to its cluster
+    fit
+        Initialize centroids and repeat the centroid-clustering functions
+        up to reach the max iterations or getting no difference in the means of each centroid
+    """
+
+    def __init__(self, X, K, mode='random', seed=None):
+
+        try:
+            self.X = X.values
+        except:
+            self.X = X
+        
+        self.K = K
+        self.mode = mode
+        self.seed = seed
+        self.max_iterations = 100
+        self.num_individuals, self.num_features = self.X.shape
+
+
+    def eucl_dist(self, a, b, axis=1):
+        return np.linalg.norm(a - b, axis=axis)
+
+    def initialize_improved_centroids(self):
+        dist = []
+        dist.append(self.X[np.random.randint(0, self.num_individuals)])
+        while len(dist)<self.K:
+            d2 = np.array([min([np.square(np.linalg.norm(i-c, None)) for c in dist]) for i in self.X])
+            prob = d2/d2.sum()
+            cum_prob = prob.cumsum()
+            r = np.random.random()
+            ind = np.where(cum_prob >= r)[0][0]
+            dist.append(self.X[ind])
+        centroids = np.array(dist)
+        self._centroids = [centroids]
+        return centroids
+
+    def initialize_random_centroids(self):
+        """Initialize K centroids at random"""
+        if self.seed is not None:
+            np.random.seed(self.seed)
+        centroids_idx = np.random.choice(range(self.num_individuals), size=self.K, replace=False)
+        centroids = self.X[centroids_idx]
+        self._centroids = [centroids]
+        return centroids
+
+
+    def create_clusters(self, centroids):
+        """Create K clusters by taking the minimal Euclidean distance between the centroids and data points
+
+        Parameters
+        ----------
+        centroids : numpy.array
+            Numpy array if K centroids to calculate K clusters using Euclidean distance
+        """
+        clusters = [[] for _ in range(self.K)]
+        
+        for point_idx, point in enumerate(self.X):
+            closest_centroid = np.argmin(np.sqrt(np.sum((point - centroids)**2, axis=1)))
+            clusters[closest_centroid].append(point_idx)
+            
+        return clusters
+
+    
+    def calculate_new_centroids(self, clusters):
+        """Realocate centroids to the center of each cluster
+
+        Parameters
+        ----------
+        clusters : list
+            List of points belonging to a cluster for calculaging the mean by cluster,
+            and therefore getting the new centroid position
+        """
+        centroids = np.zeros((self.K, self.num_features))
+        
+        for idx, cluster in enumerate(clusters):
+            new_centroid = np.mean(self.X[cluster], axis=0)
+            centroids[idx] = new_centroid
+        
+        self._centroids.append(centroids)
+        return centroids
+
+    
+    def predict_cluster(self, clusters):
+        """Label each observation to its cluster
+        
+        Parameters
+        ----------
+
+        clusters : list
+            List of clusters with point indexes
+        """
+        y_pred = np.zeros(self.num_individuals, dtype=int)
+        
+        for cluster_idx, cluster in enumerate(clusters):
+            y_pred[cluster] = cluster_idx
+        
+        return y_pred
+
+    
+    def fit(self):
+        """Initialize centroids and repeat the centroid-clustering functions
+        up to reach the max iterations or getting no difference in the means of each centroid
+        """""
+        centroids = self.initialize_random_centroids() if self.mode=="random" else self.initialize_improved_centroids()
+        self._labels = []
+        
+        for it in range(self.max_iterations):
+            clusters = self.create_clusters(centroids)
+            self._labels.append(self.predict_cluster(clusters))
+            
+            previous_centroids = centroids
+            centroids = self.calculate_new_centroids(clusters)
+            
+            diff = centroids - previous_centroids
+            
+            if not diff.any():
+                self._centroids = self._centroids[:-1]
+                break
+        
+        self.centroids = self._centroids[-1]
+        self.labels = self._labels[-1]
+        return self
+
+
+def calculate_WSS(data, target_k, max_k, mode="random"):
+    """Calculate Elbow"""
+    sse = []
+    for k in range(1, max_k+1):
+        kmeans_model = Kmeans(data, k, mode).fit()
+        curr_sse = 0
+        
+        if k == target_k:
+            model = kmeans_model
+
+        for i in range(len(data)):
+            centroid = kmeans_model.centroids[kmeans_model.labels[i]]
+            curr_sse += np.sum((data[i] - centroid) ** 2)
+
+        sse.append(curr_sse)
+    return model, sse
+
+
+def plot(model, velocidade=2):
+    """Plot animated K-means' steps
+
+    Parameters
+    ----------
+
+    model: Kmeans class
+        A Kmeans class after applying the `.fit()` method.
+    velocidade : int
+        Velocidade em segundos para transição entre frames
+    """
+    
+    # Convert velocity from seconds to miliseconds
+    velocidade*=1000 
+
+    # Set default colors
+    tab10_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+    color_dict = {i: color for i,color in enumerate(tab10_colors)}
+
+    # Get random K colors
+    if model.K <= 10:
+        palette = np.random.choice(tab10_colors, model.K, replace=False)
+    else:
+        palette = np.random.choice(tab10_colors, model.K, replace=True)
+        
+    # Set Figure Layout
+    layout = templates.plotly_dark
+    layout["showlegend"] = False
+    # layout["template"] = "plotly_dark"
+    layout["updatemenus"] = [
+        {
+            "buttons": [
+                {
+                    "args": [None, {"frame": {"duration": velocidade, "redraw": False},
+                                    "fromcurrent": True, "font": {"color":"red"},
+                                    "transition": {"duration": velocidade, "easing": "quadratic-in-out"}}
+                             ],
+                    "label": "Play",
+                    "method": "animate"
+                },
+                {
+                    "args": [[None], {"frame": {"duration": 0, "redraw": False},
+                                      "mode": "immediate",
+                                      "transition": {"duration": 0}}],
+                    "label": "Pause",
+                    "method": "animate"
+                }
+            ],
+            "font": dict(color="black"),
+            "bgcolor":"orange",
+            "direction": "left",
+            "pad": {"r": 10, "t": 87},
+            "showactive": True,
+            "type": "buttons",
+            "x": 0.1,
+            "xanchor": "right",
+            "y": 0,
+            "yanchor": "top"
+        }
+    ]
+    
+    # Set slider for animation
+    sliders_dict = {
+        "active": 0,
+        "yanchor": "top",
+        "xanchor": "left",
+        "currentvalue": {
+            "font": {"size": 20},
+            # "prefix": "Step:",
+            "visible": True,
+            "xanchor": "right"
+        },
+        "transition": {"duration": velocidade/2, "easing": "cubic-in-out"},
+        "pad": {"b": 10, "t": 50},
+        "len": 0.9,
+        "x": 0.1,
+        "y": 0,
+        "steps": []
+    }
+    
+    # Set function to add slider steps
+    def make_slider_step(i, label, value=None, redraw=False):
+        slider_step = dict(
+            args = [
+                [i],
+                {"frame": {"duration": velocidade/2, "redraw": redraw},
+                 "transition": {"duration": velocidade/2},
+                 "mode": "immediate"}
+            ],
+            label = label,
+            value = value if value is not None else label,
+            method = "animate"
+        )
+        return slider_step
+    
+    # Initialize Frames
+    frames = []
+    
+    points_init = go.Scatter(x=model.X[:,0].tolist(), y=model.X[:,1].tolist(), mode="markers", marker=dict(color="gray"), name='points')
+    centroids_init = go.Scatter(x=[], y=[], mode="markers", marker=dict(color=list(color_dict.values()), symbol='x', size=30), name='centroids')
+
+    points = go.Scatter(x=model.X[:,0].tolist(), y=model.X[:,1].tolist(), mode="markers", marker=dict(color="gray", size=10, opacity=0.8), name='points')
+    centroids = go.Scatter(x=[], y=[], mode="markers", marker=dict(color=list(color_dict.values()), symbol='x', size=30), name='centroids')
+    
+    # Add frames in loop
+    c = 0
+    for i in range(len(model._centroids)):
+
+        centroids.update(dict(x=model._centroids[i][:,0], y=model._centroids[i][:,1]))
+
+        c += 1
+        frame1 = go.Frame(data=[
+            points,
+            centroids
+        ], name=c)
+
+        frames.append(frame1)
+        sliders_dict['steps'].append(make_slider_step(c, i+1))
+
+        colors = pd.Series(model._labels[i]).map(color_dict).values
+        points.update(dict(marker=dict(color=colors)))
+
+        if i != len(model._centroids)-1:
+            c += 1
+            frame2 = go.Frame(data=[
+                points,
+                centroids
+            ], name=c)
+
+            frames.append(frame2)
+            sliders_dict['steps'].append(make_slider_step(c, ''))
+    
+    layout['sliders'] = [sliders_dict]
+    data = [points_init, centroids_init]
+
+    fig_data = {"data": data, "layout": go.Layout(layout), "frames": frames}
+    fig = go.Figure(fig_data)
+    return fig
+    
